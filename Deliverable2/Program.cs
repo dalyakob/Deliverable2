ac797594aa86eccf0f095a2a80ffe5bf406607b8
dalyakob <dalyakob@umich.edu>
2020-03-21 17:08:58 -0400

    Deliverable2 completed

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Deliverable2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string input, message = "Your encoded message is ";
+            int checkSum =0;
+
+            Console.WriteLine("What is your message?");
+            input = Console.ReadLine().ToUpper();
+
+            
+
+
+            int i = 0;
+            foreach (char temp in input)
+            {
+                message += ((int)temp - 64).ToString();
+                checkSum += (int)temp; 
+                i++;
+
+                if (i < input.Length)
+                    message += "-" ;
+            }
+
+            Console.WriteLine(message);
+            Console.WriteLine("Message checksum is " + checkSum);
+
+
+
+        }
+    }
+}
